9bfc4292352c7294c28fd83de24aec11a744554f
Matt Hensley <matt.hensley777@gmail.com>
2017-08-22 20:24:55 -0500

    json file added

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 var catContainer = document.getElementById("kat-koral");
 
-var cats = [
-{
-	name: "fluffy", 
-	color: "green", 
-	url:"https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg"
-},
-{
-	name: "john", 
-	color: "blue", 
-	url:"http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg"
-},
-{
-	name: "jones", 
-	color: "pink", 
-	url:"http://www.royalcanin.ca/~/media/Royal-Canin-Canada/Product-Categories/cat-adult-landing-hero.ashx"
-},
-];
-
 function domString (catCrap) {
 	var catString = "";
 	for(i=0; i > catCrap.length; i++) {
